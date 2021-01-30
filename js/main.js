e42d082b3fccec267fa84a4b375274d108377054
Fad91 <homyak91@inbox.ru>
2021-01-30 21:52:58 +0400

    up

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ let getRandomInt = function (min, max) {
   return Math.floor(int);
 }
 
+getRandomInt(2, 7);
+
 let getAnotherInt = function (min, max, quanity) {
-  let num = min + Math.random() * (max + 1 - min);
-  return num.toFixed(quanity);
+  let num = min + Math.random() * (max - min);
+  return +num.toFixed(quanity);
 }
+
+getAnotherInt(5, 10, 4);
